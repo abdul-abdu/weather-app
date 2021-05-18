517c65cd8f83218d2f7c871d7c0f915ec83aa5f2
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-05-18 17:25:11 +0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+const App = () => {
+	return (
+		<div className="App">
+			<h1>Weather App</h1>
+		</div>
+	);
+};
+
+export default hot(module)(App);
